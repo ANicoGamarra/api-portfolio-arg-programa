6753ec37d1a3222736b1c3541ec8f57f872fa119
ANicoGamarra <94869262+ANicoGamarra@users.noreply.github.com>
2022-04-17 12:13:16 -0300

    Update ExperienciaController.java
    agregado "CrossOrigin" y "RequestMethod"

--- src/main/java/com/ap/PorfolioApiV8/controllers/ExperienciaController.java
+++ src/main/java/com/ap/PorfolioApiV8/controllers/ExperienciaController.java
@@ -16,9 +16,12 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @RestController
 @RequestMapping("/api/experiencias")
+@CrossOrigin(origins = "*", methods= {RequestMethod.GET,RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE})
 public class ExperienciaController {
     
     @Autowired
