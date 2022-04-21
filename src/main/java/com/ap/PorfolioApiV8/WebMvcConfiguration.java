f922e3c9fd60139e923fb539c1604b649f9dcb26
Nicolas <nicogamarra2010@gmail.com>
2022-04-21 00:37:53 -0300

    cors v19

---
+++ src/main/java/com/ap/PorfolioApiV8/WebMvcConfiguration.java
@@ -0,0 +1,22 @@
+package com.ap.PorfolioApiV8;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+@Configuration
+public class WebMvcConfiguration extends WebMvcConfigurationSupport {
+
+/* (non-Javadoc)
+ * @see org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport#addCorsMappings(org.springframework.web.servlet.config.annotation.CorsRegistry)
+ */
+@Override
+protected void addCorsMappings(CorsRegistry registry) {
+    
+    registry.addMapping("/**")
+        .allowedMethods("GET", "POST", "PUT", "DELETE")
+        .allowedOrigins("*")
+        .allowedHeaders("*")
+        .allowCredentials(false);
+    }
+}
